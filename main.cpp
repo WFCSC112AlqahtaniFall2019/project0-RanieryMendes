91d7ad9dfc6fb4488b87d40ad6e8143f43030287
ranierymendes <mendrc18@wfu.edu>
2019-09-03 23:17:32 -0400

    Math equations

--- main.cpp
+++ main.cpp
@@ -3,6 +3,51 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+
+
+    int a;
+    int b;
+    int c;
+    int d;
+    int e;
+    int f;
+    cout << "Hello, User!!" << endl;
+
+    cout << "Please, enter six integer numbers" << endl;
+
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cin >> d;
+    cin >> e;
+    cin >> f;
+    cout << endl;
+
+    cout << "Your equations are: " << endl << endl << a << "X + " << b << "Y = " << c << endl;
+    cout << d << "X - " << e << "Y = " << f << endl << endl;
+
+
+    bool answer = true;
+
+    for (int i = -10; i < 11; ++i) {
+        for (int j = -10; j < 11; ++j) {
+
+            if (((a * i) + (b * j) == c) && (d * i) - (e * j) == f) {
+
+                cout << "Solution to your equation is: X = ";
+                cout << i << "  Y = " << j << endl;
+                answer = false;
+                break;
+
+            }
+
+        }
+    }
+    if (answer) {
+        cout << "No solution was found within the range -10 to 10" << endl;
+    }
+
+
     return 0;
+
 }
